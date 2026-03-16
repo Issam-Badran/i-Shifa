17e128277237a0365b34ffb33fe6ec48488f9d34
Issam Badran <issambadran3220182@gmail.com>
2026-03-16 23:22:22 +0300

    updated users migration file

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,17 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['admin', 'doctor', 'patient'])->default('patient');
+
+            // Location fields
+            $table->decimal('latitude', 10, 7)->nullable();
+            $table->decimal('longitude', 10, 7)->nullable();
+
             $table->rememberToken();
             $table->timestamps();
         });
