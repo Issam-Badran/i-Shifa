4d1835bdbac3562ebb8c053d522fbce222fa9af0
Issam Badran <issambadran3220182@gmail.com>
2026-03-18 00:10:12 +0300

    slightly updating the auth.php

--- routes/auth.php
+++ routes/auth.php
@@ -22,8 +22,9 @@ Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
     ->name('password.email');
 
 Route::post('/reset-password', [NewPasswordController::class, 'store'])
-    ->middleware('guest')
-    ->name('password.store');
+    ->middleware('guest:sanctum')
+    ->name('password.reset');
+
 
 Route::get('/verify-email/{id}/{hash}', VerifyEmailController::class)
     ->middleware(['auth', 'signed', 'throttle:6,1'])
