2b87387478a7ee7f384a3e31c2829736b824f60b
Issam Badran <issambadran3220182@gmail.com>
2026-03-17 18:02:16 +0300

    update emailVerificationNotificationController

--- app/Http/Controllers/Auth/EmailVerificationNotificationController.php
+++ app/Http/Controllers/Auth/EmailVerificationNotificationController.php
@@ -3,23 +3,26 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\JsonResponse;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class EmailVerificationNotificationController extends Controller
 {
-    /**
-     * Send a new email verification notification.
-     */
-    public function store(Request $request): JsonResponse|RedirectResponse
+    public function store(Request $request)
     {
+        // If already verified
         if ($request->user()->hasVerifiedEmail()) {
-            return redirect()->intended('/dashboard');
+            return response()->json([
+                'message' => 'Email already verified',
+                'user' => $request->user()
+            ]);
         }
 
+        // Send verification email
         $request->user()->sendEmailVerificationNotification();
 
-        return response()->json(['status' => 'verification-link-sent']);
+        return response()->json([
+            'message' => 'Verification link sent successfully',
+            'user' => $request->user()
+        ]);
     }
 }
