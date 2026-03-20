38160c66e62bd755091230f4f389ff44d14b16d8
Issam Badran <issambadran3220182@gmail.com>
2026-03-20 13:53:57 +0300

    changing guest and guest:sanctum middlewares to api.guest

--- routes/auth.php
+++ routes/auth.php
@@ -10,19 +10,19 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('api')->group(function () {
 Route::post('/register', [RegisteredUserController::class, 'store'])
-    ->middleware('guest')
+    ->middleware('api.guest')
     ->name('register');
 
 Route::post('/login', [AuthenticatedSessionController::class, 'store'])
-    ->middleware('guest:sanctum');
+    ->middleware('api.guest');
 
 
 Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
-    ->middleware('guest')
+    ->middleware('api.guest')
     ->name('password.email');
 
 Route::post('/reset-password', [NewPasswordController::class, 'store'])
-    ->middleware('guest')
+    ->middleware('api.guest')
 
     ->name('password.reset');
 
