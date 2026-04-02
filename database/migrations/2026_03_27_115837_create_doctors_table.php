f93144555e874df07095f378960383e35a429b8b
Issam Badran <issambadran3220182@gmail.com>
2026-04-02 17:50:24 +0300

    just made the specilization not nullable

--- database/migrations/2026_03_27_115837_create_doctors_table.php
+++ database/migrations/2026_03_27_115837_create_doctors_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
 
         $table->foreignId('user_id')->constrained()->onDelete('cascade');
 
-        $table->string('specialization')->nullable();
+        $table->string('specialization');
         $table->string('degree_file')->nullable();
 
         $table->time('clinic_start_time')->nullable();
